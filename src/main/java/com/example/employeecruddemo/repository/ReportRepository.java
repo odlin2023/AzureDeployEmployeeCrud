23c78fa9c9603691df0ca7a07ac8cebb652d1d22
odlin2023 <131031211+odlin2023@users.noreply.github.com>
2023-08-10 13:34:27 -0400

    first commit

---
+++ src/main/java/com/example/employeecruddemo/repository/ReportRepository.java
@@ -0,0 +1,8 @@
+package com.example.capstone.repository;
+
+import com.example.capstone.model.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
+
