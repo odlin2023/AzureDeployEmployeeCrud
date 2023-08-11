91c4eb662270feb4a9930b94514951e3f4f79341
odlin2023 <131031211+odlin2023@users.noreply.github.com>
2023-08-10 21:50:48 -0400

    second commit

---
+++ src/main/java/com/example/azureemployeedeployment/AzureEmployeeDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.azureemployeedeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureEmployeeDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureEmployeeDeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azureemployeedeployment/AzureEmployeeDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azureemployeedeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureEmployeeDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
