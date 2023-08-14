8d32f88596e08bc9a6fbe20c4b4be0a5efe54b09
odlin2023 <131031211+odlin2023@users.noreply.github.com>
2023-08-14 09:32:01 -0400

    Update NewEmployee.java

--- src/main/java/com/example/employeecruddemo/model/NewEmployee.java
+++ src/main/java/com/example/employeecruddemo/model/NewEmployee.java
@@ -1,9 +1,6 @@
-package com.example.capstone.model;
+package com.example.azureemployeedeployment.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import java.util.List;
 
 @Entity
 @Table(name = "new_employee")
